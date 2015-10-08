acecfc185f7212b245e6b204453e5482861c06e6
midun_pandera <midun.rajendran@panderasystems.com>
2015-10-08 08:04:35 -0400

    Removed json package

--- return_zindex.r
+++ return_zindex.r
@@ -2,13 +2,11 @@
 #   An R function to return  the zendex score of candidates for a Zero Chaos requisitions     #
 #   These R functions are Copyright (C) of Pandera Systems LLP                                #
 ###############################################################################################
-library(jsonlite)
 return_zindex<- function(ReqId,Score)
 {
 	ScoresJ<-Score
 	ScoresJ$requisition_id<-ReqId
 	ScoresJ$zindexscore<-with(ScoresJ,RScore+PScore+EScore)
 	colnames(ScoresJ)<-c("candidate_id","Skills","Job Fit","Experience","requisition_id","zindex_score")
-	#ScoresJ<-toJSON(ScoresJ,method="C")
 	return(ScoresJ)
 }
