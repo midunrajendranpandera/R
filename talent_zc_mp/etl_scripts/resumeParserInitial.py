e5ea63637cf92445b8262638e3aa242a65ea5a73
midun_pandera <midun.rajendran@panderasystems.com>
2016-03-09 16:38:51 -0500

    Resume Parser Sync

--- talent_zc_mp/etl_scripts/resumeParserInitial.py
+++ talent_zc_mp/etl_scripts/resumeParserInitial.py
@@ -8,30 +8,17 @@ import time
 import configparser
 from resumeResult import ResumeResult
 from parsedWords import ParsedWord
+from pymongo import MongoClient
 
 start_time = time.time()
-
-from pymongo import MongoClient
 
 config = configparser.ConfigParser()
 config.read('../common/ConfigFile.properties')
 
 uri = config.get("DatabaseSection", "database.connection_string")
 db_name = config.get("DatabaseSection", "database.dbname")
-
 client=MongoClient(uri)
 
-### QA
-#uri = "mongodb://qacandidateuser:hpbVLH6j@qamongodb01.zcdev.local:27000,qamongodb02.zcdev.local:27000,qamongodb03.zcdev.local:27000/candidate_model_qajupiter"
-#client=MongoClient(uri)
-#db=client.candidate_model_qajupiter
-#db_name = "candidate_model_qajupiter"
-
-### DEV
-#uri = "mongodb://candidateuser:bdrH94b9tanQ@devmongo01.zcdev.local:27000/?authSource=candidate_model"
-#client=MongoClient(uri)
-#db=client.candidate_model
-#db_name = "candidate_model"
 
 db = client[db_name]
 reqs = db["candidate_skills_from_parsed_resumes"]
@@ -44,25 +31,32 @@ count = 0
 cursor_start = 0
 QUERY_LIMIT = 10
 WRITE_TO_DB = False
-
-text_file = open("output_local.json", "w")
-text_file.write("[")
+printPerThousand = 1
 interpreterList = list(db.master_interpreter.find())
 total_resumes = db.candidate_resume_text.count()
-#total_resumes = 100
-print(str(total_resumes))
+#total_resumes = 7000
+print("Total Resume to be Parsed - %s" %(total_resumes))
 word_count_list = []
 wrote_first_record = False
 
 def obj_dict(self):
     return self.__dict__
 
+if(not WRITE_TO_DB):
+    text_file = open("candidate_output_local.json", "w")
+    text_file.write("[")
+
+
 while cursor_start < total_resumes:
     resumeList = list(db.candidate_resume_text.find().skip(cursor_start).limit(3500))
     for resumeLine in resumeList:
         wordcount = {}
         count += 1
-        print("Running "+ str(count))
+        printPerThousand = count % 1000
+        if(printPerThousand == 0):
+            print("Running "+ str(count))
+            print("---Time Taken: %s seconds ---" % (time.time() - start_time))
+            printPerThousand = 1
         resumeText = resumeLine["resume_text"]
         #print("Text - %s" % resumeText)
         if resumeText is not None and resumeText is not "":
@@ -92,7 +86,7 @@ while cursor_start < total_resumes:
 
             for word,match_count in wordcount.items():
                 currentResumeResult.parsedWords.append(ParsedWord(word, match_count, 0))
-             
+
             #word_count_list.append(currentResumeResult)
             ##Append the Candidate Skills to Resume
             candidateSkill = list(db.candidate.find({"candidate_id":candidateId}, {"candidate_id":1,"job_skill_names": 1,"_id":0}))
@@ -103,27 +97,30 @@ while cursor_start < total_resumes:
                     skillTemp["count"] = 1
                     skillTemp["word"] = skills["job_skill_name"].lower().strip()
                     skillTemp["interpreter_value"] = 'skills'
-                    currentResumeResult.parsedWords.append(skillTemp)                    
-            print("---Parse Time: %s seconds ---" % (time.time() - start_time))
+                    currentResumeResult.parsedWords.append(skillTemp)
+            #print("---Parse Time: %s seconds ---" % (time.time() - start_time))
             resumeJSON = json.dumps(currentResumeResult, default=obj_dict)
-            if wrote_first_record:
-                text_file.write(",")
-            else:
-                wrote_first_record = True
+            if(not WRITE_TO_DB):
+                if wrote_first_record:
+                    text_file.write(",")
+                else:
+                    wrote_first_record = True
 
-            text_file.write("%s" % resumeJSON)
-            print("count: %s - total: %s" % (count, total_resumes))
+                text_file.write("%s" % resumeJSON)
 
-            print("---JSON Time: %s seconds ---" % (time.time() - start_time))
+            #print("count: %s - total: %s" % (count, total_resumes))
+
+            #print("---JSON Time: %s seconds ---" % (time.time() - start_time))
             mongoResume = json.loads(resumeJSON)
-            print("---BSON Time: %s seconds ---" % (time.time() - start_time))
+            #print("---BSON Time: %s seconds ---" % (time.time() - start_time))
 
             if WRITE_TO_DB:
                 reqs.insert_one(mongoResume)
             #word_count_list = []
 
     cursor_start += 3500
-text_file.write("]")
-text_file.close()
+if(not WRITE_TO_DB):
+    text_file.write("]")
+    text_file.close()
 print("---Total Time: %s seconds ---" % (time.time() - start_time))
 
